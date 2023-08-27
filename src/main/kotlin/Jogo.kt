b874f961e29a575ef50611262cb6f8ced1eb9904
Matheus Rosmaninho <matheusrosmano@yahoo.com>
2023-08-27 19:13:35 -0300

    fix: Doing runCatching

--- src/main/kotlin/Jogo.kt
+++ src/main/kotlin/Jogo.kt
@@ -2,7 +2,7 @@ data class Jogo(
     val titulo: String,
     val capa: String
 ) {
-    val descricao = ""
+    var descricao: String? = null
 
     override fun toString(): String {
         return "Jogo: \n" +
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,9 @@
 import com.google.gson.Gson
-import java.lang.Exception
 import java.net.URI
 import java.net.http.HttpClient
 import java.net.http.HttpRequest
 import java.net.http.HttpResponse.BodyHandlers
-import java.util.Scanner
+import java.util.*
 
 
 fun main() {
@@ -15,7 +14,31 @@ fun main() {
     val endereco = "https://www.cheapshark.com/api/1.0/games?id=$busca"
     val client: HttpClient = HttpClient.newHttpClient()
 
-    try {
+//    try {
+//        val request = HttpRequest.newBuilder()
+//            .uri(URI.create(endereco))
+//            .build()
+//
+//        val response = client
+//            .send(request, BodyHandlers.ofString())
+//
+//        val json = response.body()
+//        println(json)
+//
+//        val gson = Gson()
+//        val meuInfoJogo = gson.fromJson(json, InfoJogo::class.java)
+//
+//        val meuJogo = Jogo(meuInfoJogo.info.title, meuInfoJogo.info.thumb)
+//
+//        println(meuJogo)
+//    } catch (ex: Exception) {
+//        println("Jogo não existe ...")
+//        println("Jogo não existe ...")
+//    }
+
+    var meuJogo: Jogo? = null
+
+    val resultado = runCatching {
         val request = HttpRequest.newBuilder()
             .uri(URI.create(endereco))
             .build()
@@ -24,17 +47,30 @@ fun main() {
             .send(request, BodyHandlers.ofString())
 
         val json = response.body()
-        println(json)
 
         val gson = Gson()
         val meuInfoJogo = gson.fromJson(json, InfoJogo::class.java)
 
-        val meuJogo = Jogo(meuInfoJogo.info.title, meuInfoJogo.info.thumb)
-
-        println(meuJogo)
-    } catch (ex: Exception) {
-        println("Jogo não existe ...")
+        meuJogo = Jogo(meuInfoJogo.info.title, meuInfoJogo.info.thumb)
     }
 
+    resultado.onFailure {
+        println("Jogo não existe")
+    }
 
+    resultado.onSuccess {
+        println("Deseja inserir uma descrição personalizada? s/n")
+        val opcao = leitura.nextLine()
+
+        if (opcao.equals("s", true)) {
+            println("Insira a descrição personalizada para o jogo:")
+            val descricaoPersonalizada = leitura.nextLine()
+            meuJogo?.descricao = descricaoPersonalizada
+        }
+        else {
+            meuJogo?.descricao = meuJogo?.titulo
+        }
+
+        println(meuJogo)
+    }
 }
