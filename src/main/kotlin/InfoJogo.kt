ba0e756be2dbefff16b8b6a35757a952c8f6366a
Matheus Rosmaninho <matheusrosmano@yahoo.com>
2023-08-27 11:04:37 -0300

    fix: Replace SerializeName by data class

--- src/main/kotlin/InfoJogo.kt
+++ src/main/kotlin/InfoJogo.kt
@@ -1,7 +1,7 @@
 import com.google.gson.annotations.SerializedName
 
 class InfoJogo(
-    @SerializedName("info") val info: Jogo
+    @SerializedName("info") val info: InfoApiShark
 ) {
     override fun toString(): String {
         return info.toString()
--- src/main/kotlin/Jogo.kt
+++ src/main/kotlin/Jogo.kt
@@ -1,8 +1,6 @@
-import com.google.gson.annotations.SerializedName
-
 class Jogo(
-    @SerializedName("title") val titulo: String,
-    @SerializedName("thumb") val capa: String
+    val titulo: String,
+    val capa: String
 ) {
     val descricao = ""
 
