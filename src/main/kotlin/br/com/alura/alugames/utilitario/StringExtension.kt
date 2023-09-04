0190fac3cd39d5560261943a10ed67559c33b357
Matheus Rosmaninho <matheusrosmano@gmail.com>
2023-09-03 22:26:23 -0300

    feat: Create extension functions

---
+++ src/main/kotlin/br/com/alura/alugames/utilitario/StringExtension.kt
@@ -0,0 +1,12 @@
+import java.time.LocalDate
+import java.time.Period
+import java.time.format.DateTimeFormatter
+
+fun String.transformarEmIdade(): Int {
+    val formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy")
+    val dataNascimento = LocalDate.parse(this, formatter)
+    val hoje = LocalDate.now()
+    val idade = Period.between(dataNascimento, hoje).years
+
+    return idade
+}
